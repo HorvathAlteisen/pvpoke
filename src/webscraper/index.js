bcc5b1a5441f41aca8f3cf6647188e8aa00f91b8
Pinstrip <kevin.pielorz@protonmail.com>
2024-08-27 22:52:20 +0200

    removed unused packages

--- src/webscraper/index.js
+++ src/webscraper/index.js
@@ -1,58 +1,50 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const express = require('express');
 const fs = require('fs');
-const zlib = require('zlib');
-
-const app = express();
 
 const url = 'https://www.pokewiki.de/Pok%C3%A9mon-Liste';
 
-app.get('/', async (req, res) => {
+async function scrapeData() {
     console.info('Scraping website...');
+
     try {
         const { data } = await axios.get(url);
         const $ = cheerio.load(data);
         const scrapedData = [];
-        let result;
 
         $('table.pwtable1 tbody').each((i, elem) => {
             elem.children
-            .filter((_, i) => i !== 0)
-            .filter((child) => $(child).text().trim() !== '')
-            .forEach((child) => {
-                const filtered = $(child).text().split('\n').filter(
-                    (_, i) => (i !== 0 && i !== 2 && i !== 9)
-                );
+                .filter((_, i) => i !== 0)
+                .filter((child) => $(child).text().trim() !== '')
+                .forEach((child) => {
+                    const filtered = $(child).text().split('\n').filter(
+                        (_, i) => (i !== 0 && i !== 2 && i !== 9)
+                    );
 
-                scrapedData.push({
-                    de: filtered[1],
-                    en: filtered[2],
-                    fr: filtered[3],
-                    jp: filtered[4],
-                    kr: filtered[5],
-                    zh: filtered[6]
+                    scrapedData.push({
+                        de: filtered[1],
+                        en: filtered[2],
+                        fr: filtered[3],
+                        jp: filtered[4],
+                        kr: filtered[5],
+                        zh: filtered[6]
+                    });
                 });
-            });
         });
 
-        result = JSON.stringify(scrapedData);
+        const result = JSON.stringify(scrapedData);
 
-        fs.writeFile('data.json', zlib.gzipSync(result).toString('base64'), (err) => {
-            if (err) {
-                console.log(err);
-            }
+        fs.writeFile('localization.json', result, (err) => {
+            err ? console.log(err) : console.info('Data written to file successfully')
         });
 
-        res.json(scrapedData);
+        console.info('Scraping finished');
+        return scrapedData;
 
     } catch (error) {
-        res.status(500).json({ error: 'Failed to scrape the website' });
+        console.error('Failed to scrape the website');
+        console.error(error);
     }
-    console.info('Scraping finished');
-});
+}
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+scrapeData();
