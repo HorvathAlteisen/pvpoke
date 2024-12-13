70a15507bfd33068d2cd53a1acd5b1392965f102
k.pielorz <k.pielorz@stiegeler.com>
2024-12-13 09:02:03 +0000

    satisfied transpiled variables

--- refactor/pvpoke/src/lib/components/Header.svelte
+++ refactor/pvpoke/src/lib/components/Header.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import {
 		WEB_HOST,
-		WEB_ROOT,
-		OG_IMAGE,
-		CANONICAL,
-		SETTINGS
+		WEB_ROOT
 	} from '$lib/config';
 	/*import Analytics from '$lib/modules/analytics.svelte';
 	import BaseCode from '$lib/modules/ads/base-code.svelte';*/
 
-	let { version, title = '', description = '' } = $props();
+	let { version, title = '', description = '', ogimage = '', canonical = '' } = $props();
 
 	// Set meta variables
 	title =
@@ -22,12 +19,17 @@
 	description =
 		description ||
 		'Looking for an edge in Pokemon GO Trainer Battles? Become a master with our open-source Pokemon battle simulator, explore the top Pokemon rankings, and get your team rated for PvP battles.';
-	let ogImage = OG_IMAGE || 'https://pvpoke.com/img/og.jpg';
+	ogimage = ogimage || 'https://pvpoke.com/img/og.jpg';
 
 	// Mock location/URI if needed for conditions (replace this with an actual route if available)
 	let locationPath = ''; // Set this to your current route as needed
 
+	/**
+	 * Temporary
+	 */
 	let menuSlideProtection = true;
+	let colorblindMode = '';
+	let SETTINGS = '';
 </script>
 
 <svelte:head>
@@ -36,14 +38,14 @@
 	<title>{title}</title>
 	<meta name="description" content={description} />
 
-	{#if CANONICAL}
-		<link rel="canonical" href={CANONICAL} />
+	{#if canonical}
+		<link rel="canonical" href={canonical} />
 	{/if}
 
 	<!--OG tags for social-->
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content={ogImage} />
+	<meta property="og:image" content={ogimage} />
 
 	<meta name="apple-mobile-web-app-capable" />
 	<meta name="mobile-web-app-capable" />
@@ -63,13 +65,13 @@
 		<link rel="stylesheet" type="text/css" href={WEB_ROOT + 'css/article-extras.css?v=21'} />
 	{/if}
 
-	{#if SETTINGS.theme && SETTINGS.theme !== 'default'}
+	<!--{#if SETTINGS.theme && SETTINGS.theme !== 'default'}
 		<link
 			rel="stylesheet"
 			type="text/css"
 			href={WEB_ROOT + 'css/themes/' + SETTINGS.theme + '.css?v=27'}
 		/>
-	{/if}
+	{/if}-->
 
 	<script src={WEB_ROOT + 'js/libs/jquery-3.3.1.min.js'}></script>
 	<script src={WEB_ROOT + 'js/interface/RSSReader.js?v=' + version}></script>
